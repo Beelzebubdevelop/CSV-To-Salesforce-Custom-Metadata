3b3b36c3744206d5e4dc1c31b783bd827bd26faf
Beelzebubdevelop <46160100+Beelzebubdevelop@users.noreply.github.com>
2021-05-03 09:24:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import csv
+from csv import DictReader
+from re import search
 from salesforceUtils import Utils
 
 sfdu = Utils('yourdevname1','test')
 sfdu.salesforce_login()
-sfdu.salesforce_retrieveCustomMetadata()
 
-with open('CSV_TO_CONVERT.csv', 'r' ) as csv_file:
-    reader = csv.DictReader(csv_file)
-    for line in reader:
-        sfdu.createXML(line)
-    print('My work here is done')
+filename = 'CSV_TO_CONVERT.csv'
+try: 
+    with open(filename, 'r' ) as csv_file:
+        sfdu.setCustomMetadataName((search('\[(.*)__mdt\]',csv_file.read())).group(1))
+        csv_file.close()
+
+    with open(filename, 'r' ) as csv_file:
+        reader = DictReader(csv_file)
+        for line in reader:
+            sfdu.createXML(line)
+        csv_file.close()
+        print('My work here is done')
+except:
+    print('GENERIC ERROR')
