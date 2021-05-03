d6e8a7d6f9575475d70f8eda14683cb80cc8ed55
Beelzebubdevelop <46160100+Beelzebubdevelop@users.noreply.github.com>
2021-05-03 08:36:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ sfdu = Utils('yourdevname1','test')
 sfdu.salesforce_login()
 sfdu.salesforce_retrieveCustomMetadata()
 
-with open('Test.csv', 'r' ) as csv_file:
+with open('CSV_TO_CONVERT.csv', 'r' ) as csv_file:
     reader = csv.DictReader(csv_file)
     for line in reader:
         sfdu.createXML(line)
